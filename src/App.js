5e36a6073465acfcd2c646368d3878df36c8c4ed
nitik1008 <nitiksuthar1219@gmail.com>
2021-09-21 19:14:03 +0530

    Creating Navigation Bar & Page Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is home page
-      </Route>
-      <Route exact path="/nitik">
-        This is nitik.
-      </Route>
-      <Route>This is 404 Page</Route>
-    </Switch>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>Not found</Route>
+      </Switch>
+    </div>
   );
 }
 
